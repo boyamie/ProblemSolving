ca967f1faacb36804bea70e97b91b3f94d4263bb
김보현 <bhkim4550@pusan.ac.kr>
2024-11-20 13:30:09 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,6 @@
+def solution(my_string):
+    lst = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    ms = []
+    ms = [ms.append(s) for s in my_string]
+    answer = [my_string.count(i) for i in lst]
+    return answer
